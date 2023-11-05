7e99bb56fcee676759962f397237cdefc272d9ad
chuck00lin <chuckmails@gmail.com>
2023-11-05 11:03:49 +0800

    subscribbe mav/local/odom as uav_odom to analyze velocity loop

--- online_03_localization.launch.py
+++ online_03_localization.launch.py
@@ -67,9 +67,9 @@ def generate_launch_description():
         name='stella_ros',
         output="screen",
         #remappings=[("/stella_ros/camera_pose","/mavros/odometry/out")],
-        arguments=["-v","./orb_vocab.fbow",
+        arguments=["--disable-mapping",
+                   "-v","./orb_vocab.fbow",
                    "-c","./runcam_720p.yaml",
-                   "--disable-mapping",
                    "--map-db-in",LaunchConfiguration('db_in')],
         parameters=[
             {"odom_frame": "odom"},
